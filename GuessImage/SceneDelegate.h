b69adfd7fb5a97024ad7b1daa16f1d226f722fa4
ikeek <ikeekcn@gmail.com>
2019-10-18 19:55:58 +0800

    Initial Commit

---
+++ GuessImage/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GuessImage
+//
+//  Created by Zhuang Yang on 2019/10/18.
+//  Copyright © 2019 Zhuang Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ GuessImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuessImage
+//
+//  Created by Zhuang Yang on 2019/10/18.
+//  Copyright © 2019 Zhuang Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
